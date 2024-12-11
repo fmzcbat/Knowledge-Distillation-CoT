5b8e5f2299ef5767effb4a9dce384fefe217ab1f
fmzcbat <128670660+fmzcbat@users.noreply.github.com>
2024-12-10 23:38:33 -0800

    Update data_loader.py to follow good practice

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,8 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
 import os
+
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
 
 
 class DataLoader:
